ab293292f6c1c51e0d8e889e811f718d02efc034
kumkum soni <kumkumsoni92@gmail.com>
2023-08-21 10:06:04 +0530

    modified:   src/app/dashboard/dashboard.component.css 	modified:   src/app/dashboard/dashboard.component.html 	modified:   src/app/dashboard/dashboard.component.ts 	new file:   src/app/dashboard/interface.ts 	modified:   src/app/home/home.component.css 	modified:   src/app/home/home.component.html 	modified:   src/app/login/login.component.css 	modified:   src/app/login/login.component.html 	modified:   src/app/nav/nav.component.css 	modified:   src/app/nav/nav.component.html 	modified:   src/app/register/register.component.css

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 import { EmployeeService } from '../employee.service';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { ColDef } from 'ag-grid-community';
+import { ColDef, GridApi, GridReadyEvent,RowSelectedEvent, SelectionChangedEvent } from 'ag-grid-community';
 import { Observable } from 'rxjs';
+import{IemployeeData} from '../dashboard/interface'
+
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,6 +14,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+ 
   // public employee:any;
 
   // constructor(private authservice:EmployeeService,private route:Router){}
@@ -26,51 +30,52 @@ export class DashboardComponent {
   //   })
   // }
   
-  // deleteEmployee(id:any){
-  //   this.authservice.deleteEmployee(id).subscribe(result=>{
-  //     console.log(result,"Data has been removed")
-  //     this.route.navigate(['/dashboard']);
-  //   },
-  //   error => {
-  //     console.log(error);
-  //   });
-  // }
+  deleteEmployee(id:any){
+    this.authservice.deleteEmployee(id).subscribe(result=>{
+      console.log(result,"Data has been removed")
+      this.route.navigate(['/dashboard']);
+    },
+    error => {
+      console.log(error);
+    });
+  }
+  //private gridApi!: GridApi<ColDef[]>;
+  private gridApi!: GridApi<IemployeeData>;
+  public guidId:any;
   
-  rowData$!: Observable<any[]>;
+  //rowData$!: Observable<any[]>;
 
   colDefs: ColDef[]=[
-    //{field:'id'},
-
-    {field:'name',checkboxSelection: true, headerCheckboxSelection: true},
-
+    {field:'id'},
+    {field:'name',checkboxSelection: false, headerCheckboxSelection: false},
     {field:'designation'},
-
     {field:'dateOfJoining'},
-
     {field:'contact'},
-
     {field:'address'},
-
     {field:'skills'},
-
     {field:'age'},
-
-    
   ];
 
   defaultColDef: ColDef={
-
     sortable:true, filter:true
-
   }
-
-  constructor(private http: HttpClient){}
-
+  public rowSelection: 'single' | 'multiple' = 'single';
+  public rowData!: IemployeeData[];
+  constructor(private http: HttpClient,private authservice:EmployeeService,private route:Router){}
+//   ngOnInit(){
+//      this.rowData$=this.http.get<any[]>('https://localhost:7175/api/EmpDetail')
+//  }
  
 
- ngOnInit(){
-
-  this.rowData$=this.http.get<any[]>('https://localhost:7175/api/EmpDetail')
-
- }
+onSelectionChanged(event: SelectionChangedEvent) {
+  const selectedRows = this.gridApi.getSelectedRows();
+  this.guidId=(document.querySelector('#selectedRows') as any).innerHTML =
+    selectedRows.length === 1 ? selectedRows[0].id: '';
+    console.log(this.guidId);
+}
+onGridReady(params: GridReadyEvent<IemployeeData>) {
+  this.gridApi = params.api;
+  this.http.get<IemployeeData[]>('https://localhost:7175/api/EmpDetail')
+    .subscribe((data) => (this.rowData = data));
+}
 }
---
+++ src/app/dashboard/interface.ts
@@ -0,0 +1,10 @@
+export interface IemployeeData {
+  id:number;
+  name: string;
+  designation: string;
+  dateOfJoining: string;
+  contact: string;
+  address: string;
+  skills: string;
+  age: number;
+}
